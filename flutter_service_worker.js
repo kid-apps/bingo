054e679976d145b13a5678b3cebed471a44aff57
Pradeep Kumaran <48370216+pradeep121212@users.noreply.github.com>
2020-04-12 19:20:36 +0530

    Add files via upload

---
+++ flutter_service_worker.js
@@ -0,0 +1,36 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "de743a87317ff1aabf73957ee2a59804",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"index.html": "8d84b7b2a6d9b2576c7a98447a3c9504",
+"/": "8d84b7b2a6d9b2576c7a98447a3c9504",
+"main.dart.js": "145de1643d3e1cc2f57aa3b365a97ca7"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
