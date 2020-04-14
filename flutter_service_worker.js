1d8e0c6de934c10d5a84c8e15e188bbac8861343
Pradeep Kumaran <48370216+pradeep121212@users.noreply.github.com>
2020-04-14 15:23:31 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "8ee90cad4379a6bb495821747647a7e8",
+  "assets/AssetManifest.json": "92d9d1459f20dbadf8ead54d68d08aeb",
 "assets/assets/button.mp3": "0842992fa9be7d1170b1af3214509458",
 "assets/assets/complete.mp3": "b3a284d0a4a216b4b0d8f395d3fe89a0",
 "assets/assets/done.mp3": "417087eee53e29def688ccc5ed41ffb8",
@@ -9,14 +9,17 @@ const RESOURCES = {
 "assets/assets/newday.mp3": "3830f7b98f845e15c3ba0194a5690437",
 "assets/assets/onepoint.mp3": "4086c172df8a6c46e63411107fe32724",
 "assets/assets/strike.mp3": "3381a980f2497b94b67c55b3cedee018",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "14238135649a8d66cb9ce4bd74702334",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"favicon.ico": "e39d2df8fe77cab5fd1377814ee176b3",
+"assets/LICENSE": "8b2654e4426a963694756e475d40b9ca",
+"favicon.ico": "3bb4e13dc6d7a29148bdeba81dbbd413",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "8d84b7b2a6d9b2576c7a98447a3c9504",
 "/": "8d84b7b2a6d9b2576c7a98447a3c9504",
-"main.dart.js": "c6b50d2f8cb0d4b9eb1c37958d42415a"
+"main.dart.js": "6bfdf9632cf6e2b468c35a87f90c8a8f",
+"manifest.json": "db10256232186578bd2e7f9ae4b2315c"
 };
 
 self.addEventListener('activate', function (event) {
